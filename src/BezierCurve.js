a04bfe332a02302987b09efc4eda1ea4f0ac088e
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2019-11-27 19:14:22 -0700

    Fixed bug with bezier curve.

--- src/BezierCurve.js
+++ src/BezierCurve.js
@@ -8,29 +8,31 @@ export default class BezierCurve {
     const reducedPoints = points.reduce((reducedPoints, point, index) => {
       if (index !== points.length - 1) {
         const nextPoint = points[index + 1];
-        reducedPoints.push( (nextPoint - point) * this.percentage);
+        reducedPoints.push((nextPoint - point) * this.percentage + point);
       }
 
       return reducedPoints;
     }, []);
 
     if (reducedPoints.length > 1) {
-      return this.reduceToPoint(result);
+      return this.reduceToPoint(reducedPoints);
     }
 
-    return reducedPoints[0] + this.points[0];
+    return reducedPoints[0];
   }
 
   valueAt(percentage) {
     this.percentage = percentage;
 
     this.validatePoints();
-    return this.reduceToPoint(this.points);
+    const value = this.reduceToPoint(this.points);
+    console.log(this.points, this.percentage, value);
+    return value;
   }
 
   validatePoints() {
-    if (this.points.length < 2){
-        throw new Error("Invalid Points: The points need to be at least two.");
+    if (this.points.length < 2) {
+      throw new Error("Invalid Points: The points need to be at least two.");
     }
 
     const controlPoints = this.points.slice(1, this.points.length - 2);
@@ -43,5 +45,4 @@ export default class BezierCurve {
       throw new Error("Invalid point: Points need to be numbers.");
     }
   }
-
 }
