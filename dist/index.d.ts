70fa73195da5f442edc541e230aec9bfda86f4d3
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2021-04-19 16:29:37 -0600

    Built and bumped version.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,8 @@
-import Animation from "./Animation";
+import Animation, { IAnimationKeyframeValue, IAnimationKeyframes, IComplexKeyframeValue } from "./Animation";
 import Player from "./Player";
 import Animator from "./Animator";
 import Keyframe from "./Keyframe";
 import easings from "./easings";
 import BezierCurve from "./BezierCurve";
 export { Animation, Player, Animator, Keyframe, BezierCurve, easings };
+export type { IAnimationKeyframeValue, IAnimationKeyframes, IComplexKeyframeValue };
