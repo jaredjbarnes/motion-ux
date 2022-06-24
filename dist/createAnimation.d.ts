06ca80b18ba8d4c500f3cd697dca407d3c90f83a
Jared Barnes <jared.barnes@tcnbroadcasting.com>
2022-06-24 14:09:35 -0600

    Fixing type yet again.

--- dist/createAnimation.d.ts
+++ dist/createAnimation.d.ts
@@ -1,7 +1,3 @@
-import { IAnimatedProperties, IPercentageKeyframes } from "./KeyframesGenerator";
+import { IAnimatedProperties } from "./KeyframesGenerator";
 import Animation from "./Animation";
-declare type ExtractType<TProps extends IAnimatedProperties<unknown>> = {
-    [P in keyof TProps]: TProps[P] extends IPercentageKeyframes<infer TP> ? TP : TProps[P];
-};
-export declare function createAnimation<T>(animatedProperties: IAnimatedProperties<T>): Animation<ExtractType<IAnimatedProperties<T>>>;
-export {};
+export declare function createAnimation<T>(animatedProperties: IAnimatedProperties<T>): Animation<T>;
--- src/createAnimation.ts
+++ src/createAnimation.ts
@@ -1,20 +1,11 @@
-import KeyframesGenerator, {
-  IAnimatedProperties,
-  IPercentageKeyframes,
-} from "./KeyframesGenerator";
+import KeyframesGenerator, { IAnimatedProperties } from "./KeyframesGenerator";
 import Animation from "./Animation";
 
 const keyframesGenerator = new KeyframesGenerator();
 
-type ExtractType<TProps extends IAnimatedProperties<unknown>> = {
-  [P in keyof TProps]: TProps[P] extends IPercentageKeyframes<infer TP>
-    ? TP
-    : TProps[P];
-};
-
 export function createAnimation<T>(animatedProperties: IAnimatedProperties<T>) {
   const keyframes = keyframesGenerator.generate<T>(animatedProperties);
-  const animation = new Animation<ExtractType<IAnimatedProperties<T>>>("", keyframes);
+  const animation = new Animation<T>("", keyframes);
 
   return animation;
 }
